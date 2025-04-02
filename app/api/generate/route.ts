539d4418c639b8087a54a79cc6056f42d3fc3f3c
aarabii <arv95464@gmail.com>
2025-04-02 23:46:47 +0530

    removed smaple music

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -74,7 +74,7 @@ export async function POST(request: Request) {
         {
           method: "POST",
           headers: {
-            Authorization: `Bearer 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`,
+            Authorization: `Bearer ${process.env.HF_TOKEN}`,
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
